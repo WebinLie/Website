d80e557fa7b4070767148f9c5b3775436a9f8b4f
WebinLie <redliwenbin31@gmail.com>
2021-12-18 10:16:52 +0000

    heroku requirements added

--- server.py
+++ server.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+    
+@app.route("/projects")
+def a():
+    return render_template('project.html')
+    
+@app.route("/about")
+def b():
+    return render_template('about.html')
